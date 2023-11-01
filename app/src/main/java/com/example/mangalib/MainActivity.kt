d4fff3ffa674658e0f118791388c9ef3d6b6ee39
Breno Lucas <brenolucas0106@gmail.com>
2023-10-31 22:06:18 -0300

    first commit

---
+++ app/src/main/java/com/example/mangalib/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mangalib
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.mangalib.screens.InicialScreen
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+        val i: Intent = Intent(baseContext, InicialScreen::class.java)
+        startActivity(i)
+    }
+}
---
+++ app/src/main/java/com/example/mangalib/screens/InicialScreen.kt
@@ -0,0 +1,12 @@
+package com.example.mangalib.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mangalib.R
+
+class InicialScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicial_screen)
+    }
+}
